0746c88d6767bbabf56f4608fe38b62a08eba621
CheaFernandez <chelsea_anne_fernandez@dlsu.edu.ph>
2023-10-19 16:01:08 +0800

    Add: AddCategory XML and Activity

---
+++ app/src/main/java/com/mobdeve/s17/charcookery/AddCategory.java
@@ -0,0 +1,36 @@
+package com.mobdeve.s17.charcookery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
+public class AddCategory extends AppCompatActivity {
+
+    EditText edit_category_title;
+    Button cancel_btn;
+    Button addcategory_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_category);
+
+        edit_category_title = findViewById(R.id.edit_category_title);
+        cancel_btn = findViewById(R.id.cancel_btn);
+        addcategory_btn = findViewById(R.id.addcategory_btn);
+    }
+    public void cancel_add(View view) {
+        edit_category_title.setText("");
+        Intent homeIntent = new Intent(AddCategory.this, MainActivity.class);
+        startActivity(homeIntent);
+
+    }
+
+    public void add_category(View view) {
+        // TODO: Add category to db and display in categories
+    }
+}

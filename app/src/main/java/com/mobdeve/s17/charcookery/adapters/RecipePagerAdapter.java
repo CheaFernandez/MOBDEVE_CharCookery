f8bdda4cf1e523ad48a149c02c71949018b96967
nicapos <67004893+nicapos@users.noreply.github.com>
2023-12-09 00:30:44 +0800

    feat: pass RecipeItem to Recipe View

--- app/src/main/java/com/mobdeve/s17/charcookery/adapters/RecipePagerAdapter.java
+++ app/src/main/java/com/mobdeve/s17/charcookery/adapters/RecipePagerAdapter.java
@@ -1,5 +1,6 @@
 package com.mobdeve.s17.charcookery.adapters;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -8,23 +9,31 @@ import com.mobdeve.s17.charcookery.IngredientsFragment;
 import com.mobdeve.s17.charcookery.InstructionsFragment;
 import com.mobdeve.s17.charcookery.NotesFragment;
 
+import java.util.ArrayList;
+
 public class RecipePagerAdapter extends FragmentPagerAdapter {
 
-    public RecipePagerAdapter(FragmentManager fm) {
+    private String[] ingredients;
+    private String[] instructions;
+    private String notes;
+
+    public RecipePagerAdapter(FragmentManager fm, String[] ingredients,
+                              String[] instructions, String notes) {
         super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.notes = notes;
     }
 
+    @NonNull
     @Override
     public Fragment getItem(int position) {
         switch (position) {
-            case 0:
-                return new IngredientsFragment();
-            case 1:
-                return new InstructionsFragment();
-            case 2:
-                return new NotesFragment();
-            default:
-                return null;
+            case 0: return new IngredientsFragment(ingredients);
+            case 1: return new InstructionsFragment();
+            case 2: return new NotesFragment();
+            default: return new Fragment();
         }
     }
 
@@ -36,14 +45,10 @@ public class RecipePagerAdapter extends FragmentPagerAdapter {
     @Override
     public CharSequence getPageTitle(int position) {
         switch (position) {
-            case 0:
-                return "Ingredients";
-            case 1:
-                return "Instructions";
-            case 2:
-                return "Notes";
-            default:
-                return null;
+            case 0: return "Ingredients";
+            case 1: return "Instructions";
+            case 2: return "Notes";
+            default: return null;
         }
     }
 }
